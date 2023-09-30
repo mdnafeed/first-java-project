df292f6c1b3de18289089a8aba4fd03530f09544
Nafeed Akram <mdnafeed08@gmail.com>
2023-09-30 20:04:48 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        int arr =10;
+        System.out.println(arr);
+
+        byte a = (byte) -189;
+        System.out.println(a);
+
+    }
+}
---
+++ src/Question1.java
@@ -0,0 +1,14 @@
+public class Question1 {
+    public static void main(String[] args){
+        int aNumber = 3;
+        if(aNumber > 0){
+            if(aNumber == 0){
+                System.out.println("First Sting");
+            }
+            else{
+                System.out.println("second String");
+            }
+            System.out.println("Third String");
+        }
+    }
+}
